103889c14eb152fe7e369419a49f79744d281f6c
maximilian98 <41920926+maximilian98@users.noreply.github.com>
2019-09-23 21:27:42 -0500

    Update polygonfill.js
    more x and y

--- scripts/polygonfill.js
+++ scripts/polygonfill.js
@@ -73,7 +73,16 @@ function DrawPolygon(polygon) {
     var vert3 = polygon.vertices[2];
     var vert4 = polygon.vertices[3];
     var vert5 = polygon.vertices[4];
-    DrawLine(vert1[0],vert1[1],vert2[0],vert2[1]);
+    
+    var x1 = vert1[0];
+    var y1 = vert1[1];
+    var x2 = vert2[0];
+    var y2 = vert2[1];
+    console.log("X1: "+x1);
+    console.log("Y1: "+y1);
+    console.log("X2: "+x2);
+    console.log("Y2: "+y2);
+    DrawLine(x1,y1,x2,y2);
     DrawLine(vert2[0],vert2[1],vert3[0],vert3[1]);
     DrawLine(vert3[0],vert3[1],vert4[0],vert4[1]);
     DrawLine(vert4[0],vert4[1],vert5[0],vert5[1]);
